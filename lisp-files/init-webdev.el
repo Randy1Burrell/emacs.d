64442e5c8dbd1f453d9923710869923956d7310c
randy1burrell <randy1burrell@yahoo.com>
2020-12-12 02:45:01 -0500

    fix: Remove repeated function definition

--- lisp-files/init-webdev.el
+++ lisp-files/init-webdev.el
@@ -48,19 +48,6 @@
 
 (when (maybe-require-package 'flycheck))
 
-;;--------------------------------------------------------------------------------
-;; configure jsx-tide checker to run after your default jsx checker
-;;--------------------------------------------------------------------------------
-(defun setup-tide-mode ()
-  "Setup function for tide."
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-
 (setq company-tooltip-align-annotations t)
 
 ;;--------------------------------------------------------------------------------
